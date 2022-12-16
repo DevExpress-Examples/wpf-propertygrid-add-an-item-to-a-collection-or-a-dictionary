90c0b1e0b0d009aabc6cc92fd3472088ea272e2f
DevExpressExampleBot <fakeEmail>
2022-12-16 17:51:18 +0400

    Source auto update [skip ci]

--- VB/MainWindow.xaml.vb
+++ VB/MainWindow.xaml.vb
@@ -1,30 +1,31 @@
-﻿Imports System.Collections.Generic
+Imports System.Collections.Generic
 Imports System.Windows
 Imports DevExpress.Mvvm.Native
 Imports DevExpress.Xpf.PropertyGrid
 
 Namespace pgrid_collection
 
-    Partial Public Class MainWindow
+    Public Partial Class MainWindow
         Inherits Window
 
         Public Sub New()
-            InitializeComponent()
-            grid.ItemsSource = New ProductList()
+            Me.InitializeComponent()
+            Me.grid.ItemsSource = New ProductList()
         End Sub
 
-        Private Sub pGrid_CellValueChanged(ByVal sender As Object, ByVal args As DevExpress.Xpf.PropertyGrid.CellValueChangedEventArgs)
-            Dim rowhandle = grid.GetSelectedRowHandles()(0)
-            grid.RefreshRow(rowhandle)
+        Private Sub pGrid_CellValueChanged(ByVal sender As Object, ByVal args As CellValueChangedEventArgs)
+            Dim rowhandle = Me.grid.GetSelectedRowHandles()(0)
+            Me.grid.RefreshRow(rowhandle)
         End Sub
     End Class
+
     Public Class ItemInitializer
         Implements IInstanceInitializer
 
         Public Sub New()
         End Sub
 
-        Private Function IInstanceInitializer_CreateInstance(ByVal type As TypeInfo) As Object Implements IInstanceInitializer.CreateInstance
+        Private Function CreateInstance(ByVal type As TypeInfo) As Object Implements IInstanceInitializer.CreateInstance
             Dim item = New Supplier()
             item.FirstName = "FIRSTNAME"
             item.LastName = "LASTNAME"
@@ -32,8 +33,7 @@ Namespace pgrid_collection
             Return item
         End Function
 
-
-        Private ReadOnly Property IInstanceInitializer_Types() As IEnumerable(Of TypeInfo) Implements IInstanceInitializer.Types
+        Private ReadOnly Property Types As IEnumerable(Of TypeInfo) Implements IInstanceInitializer.Types
             Get
                 Return New List(Of TypeInfo)() From {New TypeInfo(GetType(Supplier), "New Supplier")}
             End Get
