487fb88fa0b2a8925d0ec4d898e131855133ca9f
DevExpressExampleBot <fakeEmail>
2023-05-16 10:57:22 +0400

    Source auto update [skip ci]

--- VB/Collections/App.xaml.vb
+++ VB/Collections/App.xaml.vb
@@ -1,10 +1,12 @@
-﻿Imports System.Windows
+Imports System.Windows
 
 Namespace Collections
+
     ''' <summary>
     ''' Interaction logic for App.xaml
     ''' </summary>
     Public Partial Class App
         Inherits Application
+
     End Class
 End Namespace
--- VB/Dictionary/MainWindow.xaml.vb
+++ VB/Dictionary/MainWindow.xaml.vb
@@ -1,13 +1,17 @@
-﻿Imports DevExpress.Xpf.PropertyGrid
+Imports DevExpress.Xpf.PropertyGrid
 Imports System.Windows
+Imports System.Windows.Controls
 
 Namespace pgrid_dictionary
+
     Public Partial Class MainWindow
         Inherits Window
+
         Public Sub New()
             Me.InitializeComponent()
             Me.grid.ItemsSource = New ProductList()
         End Sub
+
         Private Sub pGrid_CellValueChanged(ByVal sender As Object, ByVal args As CellValueChangedEventArgs)
             Dim rowhandle = Me.grid.GetSelectedRowHandles()(0)
             Me.grid.RefreshRow(rowhandle)
